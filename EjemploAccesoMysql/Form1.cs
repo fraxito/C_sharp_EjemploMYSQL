8c1f8c961f78de8d60f205faaf9e0e9da0e069b2
Jorge Cisneros <fraxito@gmail.com>
2019-01-10 18:14:03 +0100

    Ejemplo de Conexion - fase 1

--- EjemploAccesoMysql/Form1.cs
+++ EjemploAccesoMysql/Form1.cs
@@ -8,13 +8,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.Odbc;
+using MySql.Data.Types;
+using MySql.Data.MySqlClient;
+
 namespace EjemploAccesoMysql
 {
     public partial class Form1 : Form
     {
+        private MySqlConnection conexion;
+        private static MySqlCommand comando;
+        private String consulta;
+        private MySqlDataReader resultado;
+        private DataTable datos = new DataTable();
+
         public Form1()
         {
             InitializeComponent();
+            //nota: si tu servidor MySQL es externo (no localhost), tienes que habilitar
+            //el acceso desde fuera
+            // en la consola de mysql o en phpmyadmin o en mysql workbench pones:
+            // grant all privileges on *.* to 'root'@'%' 
+            //si tiene contraseña pones además       identified by password ''
+            conexion = new MySqlConnection("Server = 127.0.0.1; Database = test; Uid = root; Pwd =root; Port = 3306");
+            conexion.Open();
+
+            comando = new MySqlCommand("Select * from pokemon", conexion);
+            resultado = comando.ExecuteReader();
+
+            datos.Load(resultado);
+            dataGridView1.DataSource = datos;
         }
     }
 }
